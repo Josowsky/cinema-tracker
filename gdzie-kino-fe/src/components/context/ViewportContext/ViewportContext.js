07db0d258e7ab160d212224ee2948077788352c2
Josowsky <bartosz@jozwowiak.pl>
2019-08-08 00:14:02 +0200

    Add ViewportContext

---
+++ gdzie-kino-fe/src/components/context/ViewportContext/ViewportContext.js
@@ -0,0 +1,44 @@
+import React, { createContext, useState, useEffect } from "react";
+import throttle from "lodash.throttle";
+
+import {
+  SM_MAX_WIDTH,
+  MD_MIN_WIDTH,
+  LG_MIN_WIDTH,
+  XL_MIN_WIDTH
+} from "../../../shared/constants/constants.viewport";
+
+export const ViewportContext = createContext({});
+
+const someFunction = setState =>
+  throttle(() => {
+    const viewportWidth = window.innerWidth;
+
+    setState({
+      isSmUp: viewportWidth <= SM_MAX_WIDTH,
+      isMdUp: viewportWidth >= MD_MIN_WIDTH,
+      isLgUp: viewportWidth >= LG_MIN_WIDTH,
+      isXlUp: viewportWidth >= XL_MIN_WIDTH
+    });
+  }, 150);
+
+export const ViewportContextProvider = ({ children }) => {
+  const [viewportData, setViewportData] = useState({
+    isSmUp: false,
+    isMdUp: false,
+    isLgUp: false,
+    isXlUp: false
+  });
+
+  useEffect(() => {
+    window.addEventListener("resize", someFunction(setViewportData));
+
+    return () => window.removeEventListener("resize", someFunction);
+  }, []);
+
+  return (
+    <ViewportContext.Provider value={viewportData}>
+      {children}
+    </ViewportContext.Provider>
+  );
+};
---
+++ gdzie-kino-fe/src/shared/constants/constants.viewport.js
@@ -0,0 +1,9 @@
+export const XL_MIN_WIDTH = 1201;
+
+export const LG_MAX_WIDTH = 1200;
+export const LG_MIN_WIDTH = 961;
+
+export const MD_MAX_WIDTH = 960;
+export const MD_MIN_WIDTH = 641;
+
+export const SM_MAX_WIDTH = 640;

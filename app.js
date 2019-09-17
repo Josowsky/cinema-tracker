09947ca97f420d487e6c902f7de1902c1056249c
Josowsky <bartosz@jozwowiak.pl>
2019-09-17 23:08:35 +0200

    Refactor app.js

--- app.js
+++ app.js
@@ -1,253 +1,47 @@
 const express = require('express');
 const path = require('path');
-const graphqlHTTP = require('express-graphql');
-const { buildSchema } = require('graphql');
+const errorhandler = require('errorhandler');
 
-const schema = buildSchema(`
-  type Seance {
-    date: String
-    cinema: String
-    subtitles: String
-    dimensionality: String
-    dubbing: Boolean
-    url: String
-  }
-
-  type Showing {
-    date: String
-    seances: [Seance!]
-  }
-
-  type Movie {
-    genre: String
-    id: Int!
-    image: String
-    banner: String
-    rating: Float
-    duration: String
-    name: String
-    description: String
-    showings: [Showing!]
-  }
-
-  type Query {
-    movies: [Movie!]
-    movie(id: Int): Movie
-  }
-`);
-
-const MOVIES = {
-  1: {
-    genre: 'Dramat historyczny',
-    id: 1,
-    image: 'https://images-na.ssl-images-amazon.com/images/I/51o9U06EV8L.jpg',
-    rating: 8.1,
-    name: 'Gladiator',
-    showings: [
-      {
-        date: '2019-08-09',
-        seances: [
-          {
-            date: '2019-08-09 13:00:00.000000',
-            cinema: 'Helios Poznań',
-            subtitles: false,
-            dimensionality: '2D',
-            dubbing: true,
-            url: 'https://google.com',
-          },
-          {
-            date: '2019-08-09 20:00:00.000000',
-            cinema: 'Helios Poznań',
-            subtitles: true,
-            dimensionality: '2D',
-            dubbing: false,
-            url: 'https://google.com',
-          },
-        ],
-      },
-      {
-        date: '2019-08-10',
-        seances: [
-          {
-            date: '2019-08-10 13:00:00.000000',
-            cinema: 'Helios Poznań',
-            subtitles: false,
-            dimensionality: '3D',
-            dubbing: true,
-            url: 'https://google.com',
-          },
-          {
-            date: '2019-08-10 20:00:00.000000',
-            cinema: 'Helios Poznań',
-            subtitles: false,
-            dimensionality: '3D',
-            dubbing: true,
-            url: 'https://google.com',
-          },
-        ],
-      },
-      {
-        date: '2019-08-11',
-        seances: [
-          {
-            date: '2019-08-11 13:00:00.000000',
-            cinema: 'Helios Poznań',
-            subtitles: false,
-            dimensionality: '2D',
-            dubbing: true,
-            url: 'https://google.com',
-          },
-          {
-            date: '2019-08-11 20:00:00.000000',
-            cinema: 'Helios Poznań',
-            subtitles: true,
-            dimensionality: '3D',
-            dubbing: false,
-            url: 'https://google.com',
-          },
-        ],
-      },
-      {
-        date: '2019-08-12',
-        seances: [
-          {
-            date: '2019-08-12 13:00:00.000000',
-            cinema: 'Helios Poznań',
-            subtitles: false,
-            dimensionality: '2D',
-            dubbing: true,
-            url: 'https://google.com',
-          },
-          {
-            date: '2019-08-12 20:00:00.000000',
-            cinema: 'Helios Poznań',
-            subtitles: true,
-            dimensionality: '3D',
-            dubbing: false,
-            url: 'https://google.com',
-          },
-        ],
-      },
-    ],
-  },
-  2: {
-    genre: 'Thriller',
-    id: 2,
-    image: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwix8L-GttbkAhWDlIsKHdfcCn0QjRx6BAgBEAQ&url=https%3A%2F%2Fwww.amazon.com%2FPrisoners-Poster-27-40-102cm%2Fdp%2FB00KAL2JYE&psig=AOvVaw3X4gh5yktVXuHO7DGqVZgP&ust=1568760374291457',
-    rating: 7.9,
-    name: 'Labirynt',
-    showings: [
-      {
-        date: '2019-08-09',
-        seances: [
-          {
-            date: '2019-08-09 13:00:00.000000',
-            cinema: 'Helios Poznań',
-            subtitles: false,
-            dimensionality: '2D',
-            dubbing: true,
-            url: 'https://google.com',
-          },
-          {
-            date: '2019-08-09 20:00:00.000000',
-            cinema: 'Helios Poznań',
-            subtitles: true,
-            dimensionality: '2D',
-            dubbing: false,
-            url: 'https://google.com',
-          },
-        ],
-      },
-      {
-        date: '2019-08-10',
-        seances: [
-          {
-            date: '2019-08-10 13:00:00.000000',
-            cinema: 'Helios Poznań',
-            subtitles: false,
-            dimensionality: '3D',
-            dubbing: true,
-            url: 'https://google.com',
-          },
-          {
-            date: '2019-08-10 20:00:00.000000',
-            cinema: 'Helios Poznań',
-            subtitles: false,
-            dimensionality: '3D',
-            dubbing: true,
-            url: 'https://google.com',
-          },
-        ],
-      },
-      {
-        date: '2019-08-11',
-        seances: [
-          {
-            date: '2019-08-11 13:00:00.000000',
-            cinema: 'Helios Poznań',
-            subtitles: false,
-            dimensionality: '2D',
-            dubbing: true,
-            url: 'https://google.com',
-          },
-          {
-            date: '2019-08-11 20:00:00.000000',
-            cinema: 'Helios Poznań',
-            subtitles: true,
-            dimensionality: '3D',
-            dubbing: false,
-            url: 'https://google.com',
-          },
-        ],
-      },
-      {
-        date: '2019-08-12',
-        seances: [
-          {
-            date: '2019-08-12 13:00:00.000000',
-            cinema: 'Helios Poznań',
-            subtitles: false,
-            dimensionality: '2D',
-            dubbing: true,
-            url: 'https://google.com',
-          },
-          {
-            date: '2019-08-12 20:00:00.000000',
-            cinema: 'Helios Poznań',
-            subtitles: true,
-            dimensionality: '3D',
-            dubbing: false,
-            url: 'https://google.com',
-          },
-        ],
-      },
-    ],
-  },
-};
-
-const root = {
-  movies: () => Object.keys(MOVIES).map((key) => MOVIES[key]),
-  movie: ({ id }) => MOVIES[id],
-};
+const isProduction = process.env.NODE_ENV === 'production';
 
 const app = express();
 
+app.use(require('morgan')('dev'));
+
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.use(
-  '/graphql',
-  graphqlHTTP({
-    schema,
-    rootValue: root,
-    graphiql: true,
-  }),
-);
+app.use(require('./routes'));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/client/build/index.html`));
+if (!isProduction) {
+  app.use(errorhandler());
+}
+
+
+if (!isProduction) {
+  app.use((err, req, res) => {
+    console.log(err.stack);
+
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: err,
+      },
+    });
+  });
+}
+
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+  res.json({
+    errors: {
+      message: err.message,
+      error: {},
+    },
+  });
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port);
-
-console.log(`App is listening on port ${port}`);
+const server = app.listen(process.env.PORT || 5000, () => {
+  console.log(`Listening on port ${server.address().port}`);
+});

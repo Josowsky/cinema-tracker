5d7ae22e611fa1bc7aa11f789daf164539334683
Josowsky <bartosz@jozwowiak.pl>
2019-10-12 12:40:52 +0200

    Add example spec (Header.spec)

---
+++ client/src/components/ui/Header/Header.spec.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import { Header } from "./Header";
+
+it("renders welcome message", () => {
+  const { getByText } = render(<Header />);
+  expect(getByText("???")).toBeInTheDocument();
+});

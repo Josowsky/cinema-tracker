20b99223c8e403d8e90f3195dfeb9059a719eaf5
Josowsky <bartosz@jozwowiak.pl>
2019-11-15 19:41:56 +0100

    [client] Refactor webpack.config.base.js

--- client/config/webpack/webpack.config.base.js
+++ client/config/webpack/webpack.config.base.js
@@ -1,10 +1,10 @@
-const path = require("path");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
-const reactPath = path.join(__dirname, "../../src");
-const buildPath = path.join(__dirname, "../../build");
-const publicPath = path.join(__dirname, "../../public");
-const indexFile = path.join(reactPath, "index.js");
+const reactPath = path.join(__dirname, '../../src');
+const buildPath = path.join(__dirname, '../../build');
+const publicPath = path.join(__dirname, '../../public');
+const indexFile = path.join(reactPath, 'index.tsx');
 
 module.exports = {
   reactPath,
@@ -17,25 +17,25 @@ module.exports = {
           test: /\.(png|jpg|gif|woff|woff2|eot|ttf|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
           use: [
             {
-              loader: "url-loader"
-            }
-          ]
-        }
-      ]
+              loader: 'url-loader',
+            },
+          ],
+        },
+      ],
     },
     output: {
       path: buildPath,
-      filename: "react_bundle.js"
+      filename: 'react_bundle.js',
     },
     resolve: {
-      modules: ["node_modules", reactPath],
-      extensions: [".js", ".ts", ".json", ".jsx", ".tsx"]
+      modules: ['node_modules', reactPath],
+      extensions: ['.js', '.ts', '.json', '.jsx', '.tsx'],
     },
     plugins: [
       new HtmlWebPackPlugin({
         template: `${publicPath}/index.html`,
-        filename: "index.html"
-      })
-    ]
-  }
+        filename: 'index.html',
+      }),
+    ],
+  },
 };

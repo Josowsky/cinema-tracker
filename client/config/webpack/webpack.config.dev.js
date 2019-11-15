f8b5d4423720dc1737c4e4175a1256f76ee22f24
Josowsky <bartosz@jozwowiak.pl>
2019-11-15 20:00:27 +0100

    [client] Add webpack bundle analyzer

--- client/config/webpack/webpack.config.dev.js
+++ client/config/webpack/webpack.config.dev.js
@@ -1,44 +1,48 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const baseConfig = require('./webpack.config.base');
 
-module.exports = merge(baseConfig.config, {
-  mode: 'development',
-  devtool: 'cheap-module-eval-source-map',
-  entry: ['webpack-dev-server/client?http://localhost:3000', 'webpack/hot/dev-server', baseConfig.indexFile],
-  devServer: {
-    host: 'localhost',
-    port: 3000,
-    hot: true,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
-    watchOptions: {
-      ignored: ['node_modules'],
-    },
-    proxy: {
-      '/graphql': 'http://localhost:5000',
-    },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx|ts|tsx)$/,
-        include: [baseConfig.reactPath],
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              cacheDirectory: true,
-            },
-          },
-          {
-            loader: 'eslint-loader',
-          },
-        ],
+module.exports = (env = {}) =>
+  merge(baseConfig.config, {
+    mode: 'development',
+    devtool: 'cheap-module-eval-source-map',
+    entry: ['webpack-dev-server/client?http://localhost:3000', 'webpack/hot/dev-server', baseConfig.indexFile],
+    devServer: {
+      host: 'localhost',
+      port: 3000,
+      hot: true,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
       },
-    ],
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
-});
+      watchOptions: {
+        ignored: ['node_modules'],
+      },
+      proxy: {
+        '/graphql': 'http://localhost:5000',
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx|ts|tsx)$/,
+          include: [baseConfig.reactPath],
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                cacheDirectory: true,
+              },
+            },
+            {
+              loader: 'eslint-loader',
+            },
+          ],
+        },
+      ],
+    },
+    plugins: env.analyzeBundle
+      ? [new webpack.HotModuleReplacementPlugin(), new BundleAnalyzerPlugin()]
+      : [new webpack.HotModuleReplacementPlugin()],
+  });

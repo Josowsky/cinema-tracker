b1a0f832d5ca484464bb9a1547c848a73576daf8
Josowsky <bartosz@jozwowiak.pl>
2020-01-03 00:45:31 +0100

    Pass env variables to wait-for-it from circleci

--- wait-for-it.sh
+++ wait-for-it.sh
@@ -1,12 +1,10 @@
-#!/bin/sh
-
 set -e
 
 host="$1"
 shift
 cmd="$@"
 
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "postgres" -U $POSTGRES_USER -c '\q'; do
+until PGPASSWORD=$DATABASE_PASSWORD psql -h $DATABASE_HOSTNAME -U $DATABASE_USER $DATABASE -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done

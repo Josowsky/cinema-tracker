3a3141e87e0f2492d27a9cfa4216408d9c14ef8a
Josowsky <bartosz@jozwowiak.pl>
2020-01-06 18:47:28 +0100

    Pass env variables into app image

--- wait-for-it.sh
+++ wait-for-it.sh
@@ -4,7 +4,7 @@ host="$1"
 shift
 cmd="$@"
 
-until PGPASSWORD=$DATABASE_PASSWORD psql -h $DATABASE_HOSTNAME -U $DATABASE_USER $DATABASE -c '\q'; do
+until PGPASSWORD=$DATABASE_PASSWORD psql -h 'postgres' -U $DATABASE_USER $DATABASE -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
